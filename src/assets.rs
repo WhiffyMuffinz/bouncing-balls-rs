115093e0a7d0ea312a8d9256c6575d13aaa4d9f3
Whiffy <dbene77@gmail.com>
2021-10-29 12:34:34 -0400

    re-represented vector by components, added wall collisions

--- src/assets.rs
+++ src/assets.rs
@@ -1,44 +1,43 @@
-
-
 pub struct Vector {
-    pub direction: f64,
-    pub magnitude: f64,
+    pub x: f64,
+    pub y: f64,
 }
 
 impl Vector {
-
-
     pub fn repr(&self) -> String {
-        let mut out = String::from("Vector with magnitude ");
-        out += &self.magnitude.to_string();
-        out += " and direction ";
-        out += &self.direction.to_string();
+        let mut out = String::from("Vector with components X: ");
+        out += &self.x.to_string();
+        out += "Y: ";
+        out += &self.y.to_string();
+
         out
     }
 
-    pub fn get_magnitude(&self) -> f64 {
-        self.magnitude
+    pub fn get_x(&self) -> f64 {
+        self.x
     }
 
-    pub fn get_direction(&self) -> f64 {
-        self.direction
+    pub fn get_y(&self) -> f64 {
+        self.y
     }
 
-    pub fn set_direction(&mut self, direction: f64) {
-        self.direction = direction;
+    pub fn set_x(&mut self, x: f64) {
+        self.x = x;
     }
 
-    pub fn set_magnitude(&mut self, magnitude: f64) {
-        self.magnitude = magnitude;
+    pub fn set_y(&mut self, y: f64) {
+        self.y = y;
+    }
+
+    pub fn dot(self, other: &Vector) -> f64 {
+        0.0
     }
 }
 
-
 pub struct Colour {
-    red: f64,
-    green: f64,
-    blue: f64,
-
+    pub red: f64,
+    pub green: f64,
+    pub blue: f64,
 }
 
 impl Colour {
@@ -50,11 +49,6 @@ impl Colour {
         out += " Blue: ";
         out += &self.blue.to_string();
         out
-
-    }
-
-    pub fn Copy() {
-
     }
 }
 
@@ -63,12 +57,11 @@ pub struct Ball {
     pub mass: f64,
     pub position_x: f64,
     pub position_y: f64,
-    pub vector: Vector
+    pub vector: Vector,
 }
 
-
 impl Ball {
-    pub fn repr (&self) -> String {
+    pub fn repr(&self) -> String {
         let mut out = String::from("Ball with colour: ");
         out += &self.colour.repr();
         out += ", mass: ";
@@ -79,23 +72,44 @@ impl Ball {
         out += &self.position_y.to_string();
         out += ", and ";
         out += &self.vector.repr();
+        out += ")";
 
         out
     }
 
-    pub fn get_position_x(&self) -> f64{
-        return self.position_x;
+    pub fn get_position_x(&self) -> &f64 {
+        return &self.position_x;
     }
 
-    pub fn get_position_y(&self) -> f64 {
-        return self.position_y;
+    pub fn get_position_y(&self) -> &f64 {
+        return &self.position_y;
     }
 
-    pub fn get_mass(&self) -> f64 {
-        return self.mass;
+    pub fn get_mass(&self) -> &f64 {
+        return &self.mass;
     }
 
-    //pub fn get_colour(&self) -> Colour {
-    //    return self.colour;
-    //}
+    pub fn get_colour(&self) -> &Colour {
+        return &self.colour;
+    }
+
+    pub fn handle_walls(mut self) {
+        let x = self.vector.get_x();
+        let y = self.vector.get_y();
+
+        if self.position_x < 0.0 {
+            self.vector.set_x(x * -1.0);
+        }
+        if self.position_x > 1.0 {
+            self.vector.set_x(x * -1.0);
+        }
+        if self.position_y < 0.0 {
+            self.vector.set_y(y * -1.0);
+        }
+        if self.position_y > 1.0 {
+            self.vector.set_y(y * -1.0);
+        }
+    }
+
+    pub fn collision(self, other: Ball) -> () {}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 mod assets;
 
-
 fn main() {
-    let m = assets::Vector{direction: 0.0, magnitude: 3.14};
+    let m = assets::Ball {
+        colour: assets::Colour {
+            red: 0.7,
+            green: 0.7,
+            blue: 0.7,
+        },
+        mass: 1.0,
+        position_x: 0.7,
+        position_y: 0.7,
+        vector: assets::Vector { x: 0.7, y: 0.6 },
+    };
 
-    print!("{}",m.repr());
+    print!("{}", m.repr());
 }
